05a0e4df414c39874bb5ae91d2f5fa96160e7768
Minuring <mw9505@gmail.com>
2024-12-13 11:55:53 +0900

    feat: 'Q' 입력시 종료처리
    그 외의 경우 프로그램을 반복합니다.

--- src/main/java/pairmatching/ApplicationFacade.java
+++ src/main/java/pairmatching/ApplicationFacade.java
@@ -1,10 +1,24 @@
 package pairmatching;
 
+import static pairmatching.view.input.Choice.QUIT;
+
 import pairmatching.view.InputView;
+import pairmatching.view.input.Choice;
 
 public class ApplicationFacade {
 
     public static void run() {
-        InputView.readChoice();
+        while (true) {
+            Choice choice = InputView.readChoice();
+            if (choice == QUIT) {
+                break;
+            }
+
+            handleChoice(choice);
+        }
+    }
+
+    private static void handleChoice(Choice choice) {
+        //todo : 구현
     }
 }
